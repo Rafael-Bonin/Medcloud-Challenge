83dcf60b27435963aa87dc6835a80fb8561dcedd
Rafael Bonin <rafalyf11@gmail.com>
2022-11-03 16:13:33 -0300

    feat: custom decorator for employee

--- back-end/src/employee/employee.controller.ts
+++ back-end/src/employee/employee.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Employee } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('employees')
 export class EmployeeController {
   @UseGuards(JwtGuard)
   @Get('me')
-  async getMe(@Req() req: Request) {
-    return req.user;
+  async getMe(@GetUser() user: Employee) {
+    return user;
   }
 }
